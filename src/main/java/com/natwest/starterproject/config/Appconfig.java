e6daf628428f98726447e80dc11d9a0900d5159c
Anton.Ellis <Anton.Ellis2@outlook.com>
2021-07-21 16:39:18 +0100

    New repo created for Spring Project

---
+++ src/main/java/com/natwest/starterproject/config/Appconfig.java
@@ -0,0 +1,20 @@
+package com.natwest.starterproject.config;
+
+import java.time.LocalTime;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Appconfig {
+	
+	@Bean
+	public String greetings() {
+		return "Application started at: " + LocalTime.now();
+	
+	}
+
+	
+	
+	
+}
